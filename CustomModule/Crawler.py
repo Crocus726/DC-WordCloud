d5ac29b05be41b5140a4ca028984c728ca3333f0
Crocus726 <crocus726@gmail.com>
2025-05-19 02:34:56 +0900

    Update Crawler.py
    갤러리 ID를 추가로 입력받도록 수정
    page_number 변수를 따로 만들었음

--- CustomModule/Crawler.py
+++ CustomModule/Crawler.py
@@ -11,9 +11,12 @@ def Crawl():
     # User-Agent 설정
     headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36"}
 
-    for page in range(1, int(input("1~@페이지 : ")) + 1):
+    gall_id = input("갤러리의 ID를 입력해주세요: ") # 갤러리 ID 입력받음(URL)
+    page_number = int(input("몇 페이지까지 수집하시겠습니까? (숫자만 입력): ")) # 1페이지부터 몇 페이지까지 수집할 건지 결정
+
+    for page in range(1, page_number + 1):
         # HTTP GET Request
-        response = requests.get("https://gall.dcinside.com/board/lists?id=bike&page={}".format(page), headers=headers)
+        response = requests.get("https://gall.dcinside.com/board/lists?id=" + gall_id +"&page={}".format(page), headers=headers)
 
         # response를 HTML화
         html = response.text
